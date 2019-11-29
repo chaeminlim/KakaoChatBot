a20a5691fef9d4c16149b006b720533ccbaef881
chaeminlim <dlacoals1122@naver.com>
2019-11-29 19:02:47 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+"""
+@app.route("/<route>", methods=["GET", "POST"])
+def foo():
+    pass
+def test():
+    data = jsonify(
+    version = 1.0,
+    value_list =[
+        "abc",
+        "def"
+    ])
+"""
+@app.route("/", methods = ["GET"])
+def main():
+    return "Hello u wanna got response go to /skill"
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    data = jsonify(
+    version = 1.0,
+    value_list =[
+        "abc",
+        "def"
+    ])
+    return data
+
+@app.route("/skill", methods=["POST"])
+def skill():
+    data = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText":{
+                        "text": "simple text element."
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(data)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
